9d2c7f7ed89520630b435a7309b83aa8e8c16fb9
1visible <redx.6345@gmail.com>
2021-10-12 09:02:57 +0200

    Added main method

--- Code-Nicolas/src/exercice1/Rectangle.java
+++ Code-Nicolas/src/exercice1/Rectangle.java
@@ -5,21 +5,12 @@
 package exercice1;
 
 public class Rectangle {
-	private int nb_cote, longueur, largeur, angle;
+	protected int longueur;
+	protected int largeur;
 
-	public Rectangle(int nb_cote, int longueur, int largeur, int angle) {
-		this.nb_cote = nb_cote;
+	public Rectangle(int longueur, int largeur) {
 		this.longueur = longueur;
 		this.largeur = largeur;
-		this.angle = angle;
-	}
-
-	public int getNb_cote() {
-		return nb_cote;
-	}
-
-	public void setNb_cote(int nb_cote) {
-		this.nb_cote = nb_cote;
 	}
 
 	public int getLongueur() {
@@ -38,58 +29,20 @@ public class Rectangle {
 		this.largeur = largeur;
 	}
 
-	public int getAngle() {
-		return angle;
-	}
-
-	public void setAngle(int angle) {
-		this.angle = angle;
-	}
-
 	@Override
 	public String toString() {
-		return "Rectangle{" + "nb_cote=" + nb_cote + ", longueur=" + longueur + ", largeur=" + largeur + ", angle="
-				+ angle + '}';
+		return "Rectangle{ longueur=" + longueur + ", largeur=" + largeur + " }";
 	}
 }
 
-class Carre {
-	private int nb_cotes;
-	private int longueur_cote;
-	private int angle;
+class Carre extends Rectangle {
 
-	public Carre(int nb_cotes, int longueur_cote, int angle) {
-		this.nb_cotes = nb_cotes;
-		this.longueur_cote = longueur_cote;
-		this.angle = angle;
-	}
-
-	public int getNb_cotes() {
-		return nb_cotes;
-	}
-
-	public void setNb_cotes(int nb_cotes) {
-		this.nb_cotes = nb_cotes;
-	}
-
-	public int getAngle() {
-		return angle;
-	}
-
-	public void setAngle(int angle) {
-		this.angle = angle;
-	}
-
-	public int getLongueur_cote() {
-		return longueur_cote;
-	}
-
-	public void setLongueur_cote(int longueur_cote) {
-		this.longueur_cote = longueur_cote;
+	public Carre(int longueur) {
+		super(longueur, longueur);
 	}
 
 	@Override
 	public String toString() {
-		return "Carre{" + "nb_cotes=" + nb_cotes + ", longueur_cote=" + longueur_cote + ", angle=" + angle + '}';
+		return "Carre{ longueur=" + longueur + ", largeur=" + largeur + " }";
 	}
 }
